73d26a7425ac0776f539756639f26ba9671bcc67
Pavel B <boronenkov999@mail.ru>
2023-02-04 22:11:36 +0300

    решение задачи 41 добавлен метод

--- zadacha41v2/Program.cs
+++ zadacha41v2/Program.cs
@@ -1,15 +1,15 @@
 ﻿// // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // // 0, 7, 8, -2, -2 -> 2
 // // 1, -7, 567, 89, 223-> 3
-// void PrintArray (int [] array, string [] string1)
-// {
-//   for (int i = 0; i < array.Length; i++)
-//   {
-//   array[i] = Convert.ToInt32(string1[i]);
-//   Console.Write (array[i]+"  ");
-//   }
-// Console.WriteLine();
-// }
+void PrintArray (int [] array, string [] string1)
+{
+    for (int i = 0; i < array.Length; i++)
+  {
+  array[i] = Convert.ToInt32(string1[i]);
+
+  }
+Console.WriteLine($"[{String.Join(", ", array)}]");
+}
 
 Console.Write("введите необходимое кол-во цифры(для раздедения используем пробел): ");
 string str = Console.ReadLine();
@@ -29,11 +29,7 @@ for(int i = 0; i < numbers.Length; i++)
 peremen++;
 
 int[] newnumbers = new int[peremen];
- for (int i = 0; i < newnumbers.Length; i++)
-  {
-  newnumbers[i] = Convert.ToInt32(numbers[i]);
-  }
-  Console.WriteLine($"[{String.Join(", ", newnumbers)}]");
+PrintArray(newnumbers, numbers);
 int sum = 0;
 for (int i = 0; i < newnumbers.Length; i++)
 {
@@ -42,4 +38,4 @@ for (int i = 0; i < newnumbers.Length; i++)
         sum++;
     }
 }
-Console.WriteLine($"количество значений больше 0 = {sum}");
+Console.WriteLine($"количество значений больше 0 - {sum}");
